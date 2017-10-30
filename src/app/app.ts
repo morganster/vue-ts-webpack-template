87a6aeb4deb9963f2ea47c9ba5d254c394affa85
Carlos Eduardo Jaramillo Londoño <cejl397@gmail.com>
2017-10-30 06:56:06 -0500

    fix code lint.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,17 @@
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from 'vue';
+import Component from 'vue-class-component';
 @Component({})
 export default class App extends Vue {
-  propMessage: string = "VueJS";
+  propMessage: string = 'VueJS';
   // inital data
   msg: number = 123;
   // use prop values for initial data
   helloMsg: string = 'Hello, ' + this.propMessage;
   // lifecycle hook
-  mounted() {
-    
+  mounted():void {
   }
   // method
-  greet() {
-    this.helloMsg = "nice you have change the text XD";    
+  greet():void {
+    this.helloMsg = 'nice you have change the text XD';
   }
 }
